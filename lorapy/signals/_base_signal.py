be5a5af0b3f99c469e35ac525d30c96ff649d5f3
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-03-28 14:12:12 -0700

    updated to remove inputs and return base attr

--- lorapy/signals/_base_signal.py
+++ lorapy/signals/_base_signal.py
@@ -7,15 +7,16 @@ import numpy as np
 
 class BaseLoraSignal:
 
-    def __init__(self, signal: np.array):
+    def __init__(self):
 
-        self._raw_signal = signal
+        self._raw_signal: np.array = None
 
 
 
     def __repr__(self):
         return f"{self.__class__.__name__}(length={self._raw_signal.size})"
         # TODO: may need to update to a subclassed property like self.signal or something
+
 
 
 
