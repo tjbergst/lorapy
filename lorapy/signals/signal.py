c7761373d0fb2774b16d581096d8eea892b4b901
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-03-28 14:11:10 -0700

    added import

--- lorapy/signals/signal.py
+++ lorapy/signals/signal.py
@@ -6,6 +6,7 @@ import numpy as np
 import typing as ty
 
 from lorapy.signals._base_signal import BaseLoraSignal
+from lorapy.datafile.file import DatFile  # TODO: probably will cause circular import issue
 
 
 
