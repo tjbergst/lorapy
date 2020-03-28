69a99a1c23c0af27ed4c7a74c71ad368c85c93d4
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-03-28 14:24:00 -0700

    added input for datafile and copy of required attrs

--- lorapy/signals/stats.py
+++ lorapy/signals/stats.py
@@ -1,17 +1,44 @@
 # signal stats
 
 
-from loguru import logger
-import numpy as np
-
-
+from lorapy.datafile.file import DatFile
 
 
 class SignalStats:
 
-    def __init__(self):
-        pass
+    def __init__(self, datafile: DatFile):
+
+        self._signal =          datafile.data
+        self._filename =        datafile.name
+
+        self._bw =              datafile.bw
+        self._sf =              datafile.sf
+        self._att =             datafile.att
+        self._samp_per_sym =    datafile.samp_per_sym
+        self._packet_len =      datafile.packet_len
 
 
+    @property
+    def filename(self):
+        return self._filename
 
+    @property
+    def bw(self) -> int:
+        return self._bw
+
+    @property
+    def sf(self) -> int:
+        return self._sf
+
+    @property
+    def att(self) -> int:
+        return self._att
+
+    @property
+    def samp_per_sym(self) -> int:
+        return self._samp_per_sym
+
+    @property
+    def packet_len(self) -> int:
+        return self._packet_len
 
