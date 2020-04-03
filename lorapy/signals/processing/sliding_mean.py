c1c7b37216b7b50634798c341fc2cadb0cfd10b1
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 15:45:47 -0700

    updated docstrings

--- lorapy/signals/processing/sliding_mean.py
+++ lorapy/signals/processing/sliding_mean.py
@@ -29,9 +29,29 @@ class SlidingMeanProcessor:
 
 
 
-    def _slide_and_mean(self, data_slice: np.array, overlap: float=0.5) -> int:
+
+    def _scan_slice(self, signal: np.array, slice_num: int, overlap: float=0.5) -> int:
+        """
+        calculates the start and stop indexes for the slice to be scanned and
+        then finds the minimum index via `_slide_and_mean`
+        returns the absolute index
+
+        :param signal: real abs lora signal
+        :param slice_num: iteration num used to calculate start and stop indexes
+        :param overlap: amount of overlap for `_slide_and_mean` to use
+        :return: absolute index of minimum (i.e. padding location)
+        """
+        pass
+
+
+
+    def _slide_and_mean(self, data_slice: np.array, overlap: float) -> int:
         """ iterates over `data_slice` with provided `overlap` and returns
             the start index of the window with minimum mean
+
+            :param data_slice: sliced real abs lora signal
+            :param overlap: amount of overlap to use when sliding
+            :return: index of minimum in slice (i.e. padding location)
         """
 
         step = int(overlap * self._const.padding_length)
