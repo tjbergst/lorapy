b9a28d4bbf91237f78448660e6e47f531eccd9b5
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 15:35:00 -0700

    added property for real abs signal from input, added slide and mean method to return min index

--- lorapy/signals/processing/sliding_mean.py
+++ lorapy/signals/processing/sliding_mean.py
@@ -8,12 +8,42 @@ import numpy as np
 import typing as ty
 
 from lorapy.common import constants
-
+from lorapy.signals.signal import LoraSignal  # TODO: circ import issue
 
 
 class SlidingMeanProcessor:
 
-    def __init__(self, signal: 'LoraSignal'):
-        pass
+    _const = constants
+
+    def __init__(self, signal: LoraSignal):
+
+        # signal
+        self._lora_signal = signal
+
+
+
+    @property
+    def signal(self) -> np.array:
+        return self._lora_signal.real_abs_signal
+
+
+
+
+    def _slide_and_mean(self, data_slice: np.array, overlap: float=0.5) -> int:
+        """ iterates over `data_slice` with provided `overlap` and returns
+            the start index of the window with minimum mean
+        """
+
+        step = int(overlap * self._const.padding_length)
+        indice_list = list(range(step, data_slice.size, step))
+
+        argmin = np.argmin([
+            data_slice[idx:idx + self._const.padding_length].mean()
+            for idx in indice_list
+        ])
+
+        # noinspection PyTypeChecker
+        min_index = indice_list[argmin]
+        return min_index
 
 
