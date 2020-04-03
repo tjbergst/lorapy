ca03652474421cb3ded6d020a03f8cbb5c008ffd
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 16:55:31 -0700

    added docstrings, updated endpoint pair function

--- lorapy/signals/processing/utils.py
+++ lorapy/signals/processing/utils.py
@@ -2,11 +2,17 @@
 
 from loguru import logger
 import numpy as np
+import typing as ty
 
 
-def clean_index_list(index_list: list, threshold: int = 0, shift: bool=True) -> list:
+def clean_index_list(index_list: ty.Union[list, np.array], threshold: int = 0, shift: bool=True) -> list:
     """ removed indices which are too close to one another (i.e. false padding)
         with option to shift to subsequent index
+
+        :param index_list: list of alleged padding indexes
+        :param threshold: diff threshold
+        :param shift: whether or not to return subsequent index
+        :return: list of cleaned indexes
     """
 
     index_list = np.array(index_list) if not isinstance(index_list, np.array) else index_list
@@ -19,3 +25,19 @@ def clean_index_list(index_list: list, threshold: int = 0, shift: bool=True) -> 
 
     cleaned_indexes = [idx for i, idx in enumerate(index_list) if i not in drop_indexes]
     return cleaned_indexes
+
+
+
+def generate_endpoint_pairs(index_list: ty.Union[list, np.array], packet_len: int) -> list:
+    """ generates pairs of packet endpoints based on list of padding indexes
+
+    :param index_list: list of padding indexes
+    :param packet_len: length of packet
+    :return: list of (start, stop) for each packet
+    """
+
+    return [
+        (idx, idx + packet_len) for idx in index_list
+    ]
+
+
