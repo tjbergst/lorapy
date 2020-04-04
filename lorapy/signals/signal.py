84aee20ae4d1a40318152253607bd5ecbf13e035
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 13:29:12 -0700

    added process dict, added property for process options, updated existing methods to comply

--- lorapy/signals/signal.py
+++ lorapy/signals/signal.py
@@ -8,8 +8,8 @@ import typing as ty
 from lorapy.common.utils import validate_str_option
 from lorapy.signals._base_signal import BaseLoraSignal
 from lorapy.signals.stats import SignalStats
+from lorapy.signals.processing.sliding_mean import find_all_mindices
 # from lorapy.datafile.file import DatFile  # TODO: circ import issue
-from lorapy.signals.processing.sliding_mean import SlidingMeanProcessor
 
 
 # TODO: finish process_signal
@@ -17,11 +17,14 @@ from lorapy.signals.processing.sliding_mean import SlidingMeanProcessor
 
 class LoraSignal(BaseLoraSignal):
 
-    _processing_options = ('slide-mean', 'placeholder')
-
     def __init__(self, datafile: 'DatFile'):
         # inherit
         BaseLoraSignal.__init__(self)
+
+        self._process_dict = {
+            'slide-mean':       self._process_sliding_mean,
+            '_placeholder':     None,
+        }
 
         # signal
         self._raw_signal: np.array = datafile.data[:]
@@ -38,15 +41,24 @@ class LoraSignal(BaseLoraSignal):
     def real_abs_signal(self) -> np.array:
         return np.abs(self.real_signal)
 
+    @property
+    def processing_options(self) -> list:
+        return list(self._process_dict.keys())
+
 
     def extract_packets(self) -> np.ndarray:
         """ extract all packets and return array of [packet_len, num_packets] """
         pass
 
 
-    def _process_signal(self, method: str='slide-mean'):
-        method = validate_str_option(method, self._processing_options)
+    def _process_signal(self, method: str='slide-mean', **kwargs) -> np.ndarray:
+        method = validate_str_option(method, self.processing_options)
+        logger.info(f'selected "{method}" processing method')
+
+        return self._process_dict[method](self, **kwargs)
 
 
+    def _process_sliding_mean(self, **kwargs) -> np.ndarray:
+        all_indices = find_all_mindices(self, **kwargs)
 
 
