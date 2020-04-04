d55f05d88235b7fdfc71530f0da568b83baef373
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 18:10:27 -0700

    added function to validate input string options

---
+++ lorapy/common/utils.py
@@ -0,0 +1,14 @@
+# common utilities
+
+from loguru import logger
+import typing as ty
+
+from lorapy.common import exceptions as exc
+
+
+def validate_str_option(option: str, available: ty.Collection) -> str:
+    if option not in available:
+        raise exc.InvalidOptionError(option, available)
+
+    return option
+
