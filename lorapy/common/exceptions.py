fa7fb1020e70b139e01b4a4e4ac5d75acd5551b5
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 18:09:16 -0700

    added invalid option exception

--- lorapy/common/exceptions.py
+++ lorapy/common/exceptions.py
@@ -1,4 +1,8 @@
 # lorapy exceptions
 
+from loguru import logger
 
 
+class InvalidOptionError(Exception):
+    def __init__(self, opt, available):
+        logger.error(f"invalid option '{opt}', choices are {available}")
