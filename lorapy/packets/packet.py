a4d764a8ee9f52f402e23164a2f433be1cb036ac
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 15:46:07 -0700

    added plot method with kwarg acceptance

--- lorapy/packets/packet.py
+++ lorapy/packets/packet.py
@@ -5,6 +5,8 @@ from loguru import logger
 import numpy as np
 import typing as ty
 
+import matplotlib.pyplot as plt
+
 from lorapy.packets._base_packet import BaseLoraPacket
 from lorapy.signals.stats import SignalStats  # TODO: circ import issue
 
@@ -16,8 +18,21 @@ class LoraPacket(BaseLoraPacket):
         # inherit
         BaseLoraPacket.__init__(self, data, stats)
 
+        # self.data
+        # self.stats
 
 
 
 
+
+    def plot(self, future_options: bool=False, *args, **kwargs) -> None:
+        return self._plot_packet(future_options, *args, **kwargs)
+
+
+    def _plot_packet(self, future_options: bool, *args, **kwargs) -> None:
+        """ plots packet with future options """
+        # TODO: incorporate lorapy.plotting
+
+        plt.plot(self.data, *args, **kwargs)
+        plt.show()
 
