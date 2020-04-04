92d873b962b22ed947e3a7b3ad984dc04cc9ca16
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 15:25:32 -0700

    added input for signal stats

--- lorapy/packets/packet.py
+++ lorapy/packets/packet.py
@@ -6,13 +6,17 @@ import numpy as np
 import typing as ty
 
 from lorapy.packets._base_packet import BaseLoraPacket
+from lorapy.signals.stats import SignalStats  # TODO: circ import issue
+
 
 
 class LoraPacket(BaseLoraPacket):
 
-    def __init__(self, data: np.array):
+    def __init__(self, data: np.array, stats: SignalStats):
         # inherit
-        BaseLoraPacket.__init__(self, data)
+        BaseLoraPacket.__init__(self, data, stats)
+
+
 
 
 
