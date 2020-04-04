89621ce85f678b019067406e2a9b78e8b03082ba
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 17:02:53 -0700

    set skeleton of packet slicing function

--- lorapy/packets/utils.py
+++ lorapy/packets/utils.py
@@ -1,4 +1,21 @@
 # utils for packets in lora signal
 
 
+from loguru import logger
+import numpy as np
+import typing as ty
+
+
+
+def slice_all_packets(signal: np.array, endpoints: ty.List[ty.Tuple[int, int]]) -> np.ndarray:
+    max_length = max(stop - start for start, stop in endpoints)
+    logger.debug(f'got max packet length: {max_length}')
+
+    _packets = np.vstack([
+        _ for pair in endpoints
+    ])
+
+    logger.info(f'extracted {len(_packets)} packets from signal')
+    return _packets
+
 
