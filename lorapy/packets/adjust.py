1660925c169c72a0a85503aa8fb29ddc2ae385ed
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 17:56:11 -0700

    set additional imports

--- lorapy/packets/adjust.py
+++ lorapy/packets/adjust.py
@@ -6,11 +6,12 @@ import numpy as np
 import typing as ty
 
 from lorapy.common import constants
+from lorapy.signals.signal import LoraSignal  # TODO: circ import issue
 
 
 class PacketAdjuster:
 
-    def __init__(self):
+    def __init__(self, signal: LoraSignal):
         pass
 
 
