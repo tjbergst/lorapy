ba5e41d39f6e665a175d12cf1a623a683366f21b
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 15:23:10 -0700

    updated repr to also show signal stats

--- lorapy/packets/_base_packet.py
+++ lorapy/packets/_base_packet.py
@@ -1,15 +1,17 @@
 # base lora packet
 
 import numpy as np
+from lorapy.signals.stats import SignalStats  # TODO: circ import issue
 
 
 class BaseLoraPacket:
 
-    def __init__(self, data: np.array):
+    def __init__(self, data: np.array, stats: SignalStats):
 
         self.data = data
+        self.stats = stats
 
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(length={self.data.size})"
+        return f"{self.__class__.__name__}(length={self.data.size}) | {self.stats}"
 
