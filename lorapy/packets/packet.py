e022eb59962d02906bfb3cbef3545b060df34ba9
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 15:15:16 -0700

    updated inheritance of base packet class

--- lorapy/packets/packet.py
+++ lorapy/packets/packet.py
@@ -5,11 +5,15 @@ from loguru import logger
 import numpy as np
 import typing as ty
 
+from lorapy.packets._base_packet import BaseLoraPacket
+
+
+class LoraPacket(BaseLoraPacket):
+
+    def __init__(self, data: np.array):
+        # inherit
+        BaseLoraPacket.__init__(self, data)
 
 
 
-class LoraPacket:
-
-    def __init__(self):
-        pass
 
