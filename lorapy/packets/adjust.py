c15a968e29b55ab03cdaab3777897da27862929f
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-03 17:45:46 -0700

    set initial class structure

--- lorapy/packets/adjust.py
+++ lorapy/packets/adjust.py
@@ -1,3 +1,16 @@
 # packet adjusting for lora signal packets
 
 
+from loguru import logger
+import numpy as np
+import typing as ty
+
+from lorapy.common import constants
+
+
+class PacketAdjuster:
+
+    def __init__(self):
+        pass
+
+
