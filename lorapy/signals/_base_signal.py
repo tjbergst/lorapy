4f11d2cd83c015e60003ff91843a20ce78d55dd4
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 18:24:29 -0700

    added prop for number of packets

--- lorapy/signals/_base_signal.py
+++ lorapy/signals/_base_signal.py
@@ -5,20 +5,19 @@ import numpy as np
 
 
 
+
 class BaseLoraSignal:
 
     def __init__(self):
 
         self._raw_signal: np.array = None
-
+        self.packets = None
 
 
     def __repr__(self):
         return f"{self.__class__.__name__}(length={self._raw_signal.size})"
         # TODO: may need to update to a subclassed property like self.signal or something
 
-
-
-
-
-
+    @property
+    def num_packets(self) -> int:
+        return len(self.packets)
