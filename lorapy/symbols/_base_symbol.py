a9f0b7bd66f795d75113232045ca46529e1996af
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 11:45:01 -0700

    new for lora symbols

---
+++ lorapy/symbols/_base_symbol.py
@@ -0,0 +1,11 @@
+# base lora symbol
+
+
+from lorapy.signals.stats import SignalStats  # TODO: circ import issue
+
+
+class BaseLoraSymbol:
+
+    def __init__(self):
+
+        pass
---
+++ lorapy/symbols/symbol.py
@@ -0,0 +1,13 @@
+# lora symbol
+
+
+from loguru import logger
+import numpy as np
+import typing as ty
+
+from lorapy.common import constants
+
+
+
+class LoraSymbol:
+    pass
