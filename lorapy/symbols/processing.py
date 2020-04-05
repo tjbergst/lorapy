8562548f0faff31f4aaaa8081c22b8b73b648bbe
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 13:01:38 -0700

    added simple endpoint generator function

--- lorapy/symbols/processing.py
+++ lorapy/symbols/processing.py
@@ -1,3 +1,16 @@
 # lora symbol processing
 
+import numpy as np
 
+
+def generate_symbol_endpoints(num_symbols: int, samp_per_sym: int) -> list:
+    """ generates endpoints for symbols within packet """
+
+    endpoints = []
+    start, stop = 0, samp_per_sym
+
+    for i in range(num_symbols):
+        endpoints.append((start, stop))
+        start, stop = stop, stop + samp_per_sym
+
+    return endpoints
