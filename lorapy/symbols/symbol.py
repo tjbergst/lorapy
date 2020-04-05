a9c73ad44036390c30fdf197890a69a440960584
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 12:39:08 -0700

    set up inheritance

--- lorapy/symbols/symbol.py
+++ lorapy/symbols/symbol.py
@@ -6,8 +6,16 @@ import numpy as np
 import typing as ty
 
 from lorapy.common import constants
+from lorapy.common.stats import LoraStats  # TODO: circ import issue
+from lorapy.symbols._base_symbol import BaseLoraSymbol
+
+
+class LoraSymbol(BaseLoraSymbol):
+
+    def __init__(self, data: np.array, stats: LoraStats, symbol_id: int, endpoints: ty.Tuple[int, int]):
+        # inherit
+        BaseLoraSymbol.__init__(self, data, stats, symbol_id, endpoints)
+        # self.stats, self.data, self.real_abs_data, self.sid
 
 
 
-class LoraSymbol:
-    pass
