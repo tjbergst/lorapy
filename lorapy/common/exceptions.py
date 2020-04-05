bc5a9ca21022b17e112ea91a4b06f8f0d1351207
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 17:02:34 -0700

    added over adjustment exception

--- lorapy/common/exceptions.py
+++ lorapy/common/exceptions.py
@@ -1,8 +1,19 @@
 # lorapy exceptions
 
 from loguru import logger
+from textwrap import dedent
 
 
 class InvalidOptionError(Exception):
     def __init__(self, opt, available):
         logger.error(f"invalid option '{opt}', choices are {available}")
+
+
+class OverAdjustedPacketError(Exception):
+    def __init__(self, adjustment, limit):
+        logger.error(dedent(f'''
+        packet adjustment set over limit [{adjustment} / {limit}]
+        this probably means the auto adjustment params `_auto_adj_look_ahead` and/or `_auto_adj_threshold`
+        are not set correctly 
+        if the packet has no trailing padding these may need to be tightened up or `auto_adjust` turned off
+        '''))
