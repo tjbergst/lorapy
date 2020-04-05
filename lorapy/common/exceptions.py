b0be5c97a0b9dced3d3a5e14040b0cecf532d1c4
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-04 17:10:44 -0700

    clarified error message

--- lorapy/common/exceptions.py
+++ lorapy/common/exceptions.py
@@ -13,7 +13,10 @@ class OverAdjustedPacketError(Exception):
     def __init__(self, adjustment, limit):
         logger.error(dedent(f'''
         packet adjustment set over limit [{adjustment} / {limit}]
-        this probably means the auto adjustment params `_auto_adj_look_ahead` and/or `_auto_adj_threshold`
-        are not set correctly 
+
+        look_ahead = `cls._auto_adj_look_ahead` if `auto_adjust` else `look_ahead`
+        threshold = `cls._auto_adj_threshold` if `auto_adjust` else `threshold`
+
+        this probably means the adjustment params `look_ahead` and/or `threshold` are not set correctly 
         if the packet has no trailing padding these may need to be tightened up or `auto_adjust` turned off
         '''))
