e28e689d015ee6b1256e5e77f0c0bd2fb63e6621
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 13:52:15 -0700

    added processing utils

--- lorapy/symbols/symbol.py
+++ lorapy/symbols/symbol.py
@@ -7,14 +7,17 @@ import typing as ty
 
 from lorapy.common.stats import LoraStats  # TODO: circ import issue
 from lorapy.symbols._base_symbol import BaseLoraSymbol
+from lorapy.symbols import processing
 
 
 class LoraSymbol(BaseLoraSymbol):
+
+    _proc = processing
 
     def __init__(self, data: np.array, stats: LoraStats, symbol_id: int, endpoints: ty.Tuple[int, int]):
         # inherit
         BaseLoraSymbol.__init__(self, data, stats, symbol_id, endpoints)
         # self.stats, self.data, self.real_abs_data, self.sid
 
-
+        #
 
