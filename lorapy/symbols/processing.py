3d96edb6951d9357a1b09e2bb04362d3e0594289
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 13:06:35 -0700

    added simplified listcomp method for generating endpoints

--- lorapy/symbols/processing.py
+++ lorapy/symbols/processing.py
@@ -1,16 +1,12 @@
 # lora symbol processing
 
-import numpy as np
 
 
-def generate_symbol_endpoints(num_symbols: int, samp_per_sym: int) -> list:
-    """ generates endpoints for symbols within packet """
-
-    endpoints = []
-    start, stop = 0, samp_per_sym
-
-    for i in range(num_symbols):
-        endpoints.append((start, stop))
-        start, stop = stop, stop + samp_per_sym
-
-    return endpoints
+def generate_preamble_endpoints_lc(num_symbols: int, samp_per_sym: int) -> list:
+    return [
+        (start, stop)
+        for start, stop in zip(
+            range(0, num_symbols * samp_per_sym, samp_per_sym),
+            range(samp_per_sym, num_symbols * samp_per_sym + samp_per_sym, samp_per_sym)
+        )
+    ]
