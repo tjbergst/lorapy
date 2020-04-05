8910166a8794ab64af4938f0cf747227c191b350
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 13:40:20 -0700

    moved functions into new utils

--- lorapy/symbols/processing.py
+++ lorapy/symbols/processing.py
@@ -1,12 +1,1 @@
 # lora symbol processing
-
-
-
-def generate_preamble_endpoints_lc(num_symbols: int, samp_per_sym: int) -> list:
-    return [
-        (start, stop)
-        for start, stop in zip(
-            range(0, num_symbols * samp_per_sym, samp_per_sym),
-            range(samp_per_sym, num_symbols * samp_per_sym + samp_per_sym, samp_per_sym)
-        )
-    ]
