0ec99013e1497d85ec95909c8a0ba0f4658f6f28
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 13:34:24 -0700

    added function to filter file generator based on string matches in file name

--- lorapy/common/paths.py
+++ lorapy/common/paths.py
@@ -7,3 +7,8 @@ import typing as ty
 def glob_files(path: pathlib.Path, pattern: str='**/*.dat') -> ty.Generator:
     yield from (file for file in path.glob(pattern))
 
+
+
+def filter_file_generator(filegen: ty.Generator, match_value: str) -> ty.Generator:
+    return (file for file in filegen if match_value in file.name)
+
