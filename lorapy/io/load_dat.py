db4428f8bece345337fd3d1f743c1c567d7047c4
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 14:17:08 -0700

    added test .p file loader

--- lorapy/io/load_dat.py
+++ lorapy/io/load_dat.py
@@ -23,6 +23,15 @@ class DatLoader(BaseLoader):
 
 
 
+class DotPLoader(BaseLoader):
+
+    _glob_pattern = '**/*.p'
+    _file_class = DatFile
+
+    def __init__(self, data_path: ty.Union[pathlib.Path, str],
+                 autoload: bool=True, glob_pattern: ty.Optional[str]=None):
+        # inherit
+        BaseLoader.__init__(self, data_path, autoload, glob_pattern)
 
 
 
