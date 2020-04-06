8fbc0818b0809db115e12a5d0c72146c8925ec03
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 15:40:54 -0700

    set up basic intake for dot p file data

--- lorapy/symbols/baseline.py
+++ lorapy/symbols/baseline.py
@@ -1,3 +1,19 @@
 # lora baseline symbol for convolve comparison
 
 
+import numpy as np
+
+from lorapy.datafile.file import DotPFile
+
+
+
+class BaselineSymbol:
+
+    def __init__(self, dot_p: DotPFile):
+
+        self.data = dot_p.data
+
+
+
+
+
