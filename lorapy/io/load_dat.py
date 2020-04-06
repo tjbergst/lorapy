d6c053a0fca94e73daa66393103c48c4c1c4d1d4
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 14:03:29 -0700

    removed unnecessary attrs and imports

--- lorapy/io/load_dat.py
+++ lorapy/io/load_dat.py
@@ -1,10 +1,8 @@
 # load datafile
 
-from loguru import logger
 import pathlib
 import typing as ty
 
-from lorapy.common import paths
 from lorapy.io._base_loader import BaseLoader
 from lorapy.datafile.file import DatFile
 
@@ -15,14 +13,11 @@ class DatLoader(BaseLoader):
     _glob_pattern = '**/*.dat'
     _file_class = DatFile
 
-    _path_utils = paths
-
     def __init__(self, data_path: ty.Union[pathlib.Path, str],
                  autoload: bool=True, glob_pattern: ty.Optional[str]=None):
         # inherit
         BaseLoader.__init__(self, data_path, autoload, glob_pattern)
 
-        logger.info('initialized DatLoader')
 
 
 
