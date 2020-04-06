b4d15e95acbb8b08682fa085cbbc317b513eb68a
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 12:29:56 -0700

    created new base loader class

---
+++ lorapy/io/_base_loader.py
@@ -0,0 +1,21 @@
+# base file loader
+
+
+from loguru import logger
+import pathlib
+import typing as ty
+
+from lorapy.common import paths
+
+
+
+
+class BaseLoader:
+
+    _path_utils = paths
+
+    def __init__(self):
+
+        pass
+
+
