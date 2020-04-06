d58f055f28c63239748874ba0052f1063e3174b0
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-06 13:07:02 -0700

    added file id input to loading

--- lorapy/io/_base_loader.py
+++ lorapy/io/_base_loader.py
@@ -48,7 +48,7 @@ class BaseLoader:
         _file_generator = self._path_utils.glob_files(self.data_dir, self._glob_pattern)
 
         if self._autoload:
-            return (self._load_file(path) for path in _file_generator)
+            return (self._load_file(path, idx) for idx, path in enumerate(_file_generator))
 
         return _file_generator
 
@@ -76,7 +76,7 @@ class BaseLoader:
 
         if path.is_file():
             logger.info(f'validated datafile file at {path}')
-            self.data_file = self._load_file(path) if self._autoload else path
+            self.data_file = self._load_file(path, file_id=0) if self._autoload else path
             return None
 
         logger.debug(f'set datafile directory: {path}')
@@ -93,10 +93,10 @@ class BaseLoader:
             raise FileNotFoundError(f'no datafile files [{self._glob_pattern}] found in directory {self.data_dir}')
 
         logger.info(f'found {len(self.file_list)} data file(s)')
-        self.data_file = self._load_file(self.file_list[0]) if self._autoload else self.file_list[0]
+        self.data_file = self._load_file(self.file_list[0], file_id=0) if self._autoload else self.file_list[0]
 
 
-    def _load_file(self, path: pathlib.Path):
+    def _load_file(self, path: pathlib.Path, file_id: int):
         """ subclass specific data file """
-        return self._file_class(path)
+        return self._file_class(path, file_id)
 
