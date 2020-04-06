380b28a60f7600eaf02c4b96cfa9f0775cd59a5f
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 17:21:57 -0700

    added function to convolve symbol with baseline

--- lorapy/symbols/convolution.py
+++ lorapy/symbols/convolution.py
@@ -5,3 +5,9 @@ import typing as ty
 
 
 
+def convolve_symbols(baseline: np.array, symbol: np.array, symbol_conj: np.array) -> float:
+    conv_val = (
+        np.abs(np.convolve(baseline, symbol_conj)) / np.linalg.norm(symbol) ** 2
+    )
+
+    return conv_val
