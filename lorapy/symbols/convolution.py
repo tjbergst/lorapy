a41687fc67f46f65cdb9de8d0017881e92b866b1
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-05 17:24:54 -0700

    added arg and expression to return max if desired

--- lorapy/symbols/convolution.py
+++ lorapy/symbols/convolution.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 
-def convolve_symbols(baseline: np.array, symbol: np.array, symbol_conj: np.array) -> float:
+def convolve_symbols(baseline: np.array, symbol: np.array, symbol_conj: np.array, _max: bool=True) -> float:
     conv_val = (
         np.abs(np.convolve(baseline, symbol_conj)) / np.linalg.norm(symbol) ** 2
     )
 
-    return conv_val
+    return conv_val.max() if _max else conv_val
