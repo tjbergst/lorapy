88ea2b25365e9c7c2e4efba7fc02c4a4c258d91a
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-08 12:14:47 -0700

    updated args to match new utils, added todo, added docstrings for init and locate

--- lorapy/packets/symbol_locations.py
+++ lorapy/packets/symbol_locations.py
@@ -16,6 +16,15 @@ class SymbolLocator:
     _sym_utils = utils
 
     def __init__(self, packet: LoraPacket, range_factor: int=10, step: int=2, scalar: float=0.6, dev: bool=True):
+        """
+        Finds lora preamble symbols by computing the correlation with a baseline symbol along
+        a sliding window of packet data. Tunable params are discussed below.
+
+        :param packet: LoraPacket
+        :param range_factor: how far into packet to scan in terms of symbol length (i.e. `range_factor` * `samp_per_sym`)
+        :param step: window step size
+        :param scalar: scalar threshold for finding peaks in list of correlation value (i.e. `scalar` * max(`corr_vals`)
+        """
 
         self._dev = dev
 
@@ -38,11 +47,18 @@ class SymbolLocator:
         return self.symbol.data
 
 
-    def locate_symbols(self, baseline_symbol: BaselineSymbolSet, preamble_only: bool=True,
-                       range_factor: ty.Optional[int]=None, step: ty.Optional[int]=None,
-                       scalar: ty.Optional[int]=None) -> list:
-        if not preamble_only:
-            raise NotImplementedError('only preamble symbol location implemented at this time')
+    def locate_symbols(self, baseline_symbol: BaselineSymbolSet, range_factor: ty.Optional[int]=None,
+                       step: ty.Optional[int]=None, scalar: ty.Optional[int]=None) -> list:
+        """
+        Finds lora preamble symbols by computing the correlation with a baseline symbol along
+        a sliding window of packet data. Tunable params are discussed below.
+
+        :param baseline_symbol: known Lora preamble symbol for given BW, SF
+        :param range_factor: how far into packet to scan in terms of symbol length (i.e. `range_factor` * `samp_per_sym`)
+        :param step: window step size
+        :param scalar: scalar threshold for finding peaks in list of correlation value (i.e. `scalar` * max(`corr_vals`)
+        :return: list of peak correlations
+        """
 
         # set variables
         self.symbol = baseline_symbol
@@ -61,8 +77,10 @@ class SymbolLocator:
         corr_vals = self._compute_correlation_values(samp_per_sym, shifts)
         corr_threshold = self._sym_utils.set_corr_threshold(corr_vals, scalar)
 
-        peak_shifts = self._sym_utils.find_peak_shifts(corr_vals, corr_threshold, shifts, first=False)
-        self._create_sanity_plot(corr_vals, peak_shifts)
+        peak_shifts = self._sym_utils.find_peak_shifts(
+            corr_vals, corr_threshold, shifts, first=False, sanity_plot=self._dev,
+        )
+        # TODO: add list cleaning (i.e. min distance of samp_per_sym between peaks)
         return peak_shifts
 
 
@@ -77,21 +95,5 @@ class SymbolLocator:
             plt.show();
 
         return correlations
-
-
-    def _create_sanity_plot(self, corr_vals: list, peak_shifts: list) -> None:
-        if not self._dev:
-            return
-
-        # creates a list of spikes at peak shift locations
-        packet_strips = [
-            np.max(corr_vals) * 1.2 if idx in peak_shifts else 0
-            for idx, _ in enumerate([0] * len(corr_vals))
-        ]
-
-        plt.plot(corr_vals);
-        plt.plot(packet_strips);
-        plt.title(f'symbol location overlay');
-        plt.show();
 
 
