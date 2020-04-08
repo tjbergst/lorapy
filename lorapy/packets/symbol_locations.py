dc1b822b1502e96ec940a0c367786559b7e56460
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-08 11:08:18 -0700

    removed static methods and external functions, renamed class more appropriately

--- lorapy/packets/symbol_locations.py
+++ lorapy/packets/symbol_locations.py
@@ -9,16 +9,14 @@ from lorapy.packets.packet import LoraPacket
 
 
 
-class LoraPacketSyncer:
+class SymbolLocator:
 
     _range_factor = 10
 
-    def __init__(self, baseline_symbol: BaselineSymbolSet, packet: LoraPacket):
+    def __init__(self, packet: LoraPacket):
 
-        self.symbol = baseline_symbol
         self.packet = packet
-
-
+        self.symbol = None
 
 
     @property
@@ -30,45 +28,15 @@ class LoraPacketSyncer:
         return self.symbol.data
 
 
-    def shift_and_correlate(self, base_symbol: np.ndarray,
-                            packet: np.ndarray, samp_per_sym: int, shifts: range) -> list:
-        corr_vals = [
-            self._compute_corrcoefs(base_symbol, packet[shift: shift + samp_per_sym - 1])
-            for shift in shifts
-        ]
+    def locate_symbols(self, preamble_only: bool=True) -> list:
+        if not preamble_only:
+            raise NotImplementedError('only preamble symbol location implemented at this time')
 
-        return corr_vals
+        return self._locate_symbols()
 
 
-    @staticmethod
-    def _compute_corrcoefs(base_symbol: np.ndarray, packet_slice: np.ndarray) -> float:
-        return np.real(np.abs(
-            np.corrcoef(base_symbol, packet_slice)[0, 1]
-        ))
+    def _locate_symbols(self) -> list:
+        pass
 
 
-
-
-def _find_first_peak(corr_vals: list, threshold: float, shifts: range):
-    peaks = np.where(corr_vals > threshold)[0]
-    logger.debug(f'found {len(peaks)} peaks [{peaks[0]}]')
-
-    shift = list(shifts)[peaks[0]]
-    return shifts
-
-
-def _generate_shifts(samples_per_sym: int,
-                     range_factor: int = 10, step: int = 2) -> range:
-    return range(0, int(samples_per_sym * range_factor), step)
-
-
-def _determine_max_correlation_shift(corr_vals: list, shifts: range) -> int:
-    # noinspection PyTypeChecker
-    argmax: int = np.argmax(corr_vals)
-    return list(shifts)[argmax]
-
-
-def set_corr_threshold(corr_vals: list, scalar: float = 0.6):
-    threshold = np.max(corr_vals) * 0.60
-    return threshold
 
