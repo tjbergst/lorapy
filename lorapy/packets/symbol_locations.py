6bd3888cb215861ebcb07c2e6291a8989d8b2039
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-08 11:28:09 -0700

    split correlation values into separate method and added dev plots, added init input for dev

--- lorapy/packets/symbol_locations.py
+++ lorapy/packets/symbol_locations.py
@@ -3,6 +3,7 @@
 from loguru import logger
 import numpy as np
 import typing as ty
+import matplotlib.pyplot as plt
 
 from lorapy.symbols.baseline import BaselineSymbolSet
 from lorapy.packets.packet import LoraPacket
@@ -14,11 +15,16 @@ class SymbolLocator:
 
     _sym_utils = utils
 
-    _range_factor = 10
+    def __init__(self, packet: LoraPacket, range_factor: int=10, step: int=2, dev: bool=True):
 
-    def __init__(self, packet: LoraPacket):
+        self._dev = dev
 
         self.packet = packet
+
+        # TODO: add variable steps per BW in constants?
+        self._range_factor = range_factor
+        self._step = step
+
         self.symbol = None
 
 
@@ -31,15 +37,38 @@ class SymbolLocator:
         return self.symbol.data
 
 
-    def locate_symbols(self, preamble_only: bool=True) -> list:
+    def locate_symbols(self, baseline_symbol: BaselineSymbolSet, preamble_only: bool=True,
+                       range_factor: ty.Optional[int]=None, step: ty.Optional[int]=None) -> list:
         if not preamble_only:
             raise NotImplementedError('only preamble symbol location implemented at this time')
 
-        return self._locate_symbols()
+        # set variables
+        self.symbol = baseline_symbol
+        range_factor = range_factor if range_factor is not None else self._range_factor
+        step = step if step is not None else self._step
+        samp_per_sym = self.packet.stats.samp_per_sym
+
+        return self._locate_symbols(samp_per_sym, range_factor, step)
 
 
-    def _locate_symbols(self) -> list:
-        pass
+    def _locate_symbols(self, samp_per_sym: int, range_factor: int, step: int) -> list:
+        corr_vals = self._compute_correlation_values(samp_per_sym, range_factor, step)
+
+
+
+    def _compute_correlation_values(self, samp_per_sym: int, range_factor: int, step: int) -> list:
+        shifts = self._sym_utils.generate_shifts(samp_per_sym, range_factor, step)
+
+        correlations = self._sym_utils.shift_and_correlate(
+            self.symbol_data, self.packet_data, samp_per_sym, shifts,
+        )
+
+        if self._dev:
+            plt.plot(correlations);
+            plt.title(f'samp per sym: {samp_per_sym} | range factor: {range_factor} | step: {step}');
+            plt.show();
+
+        return correlations
 
 
 
