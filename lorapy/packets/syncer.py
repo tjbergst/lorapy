4e3398ec3282ed918d93315f6d7e286e7635a597
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-07 20:23:33 -0700

    added intake and basic properties

--- lorapy/packets/syncer.py
+++ lorapy/packets/syncer.py
@@ -4,13 +4,29 @@ from loguru import logger
 import numpy as np
 import typing as ty
 
+from lorapy.symbols.baseline import BaselineSymbolSet
+from lorapy.packets.packet import LoraPacket
 
 
 
 class LoraPacketSyncer:
 
-    def __init__(self):
-        pass
+    def __init__(self, baseline_symbol: BaselineSymbolSet, packet: LoraPacket):
+
+        self.symbol = baseline_symbol
+        self.packet = packet
+
+
+
+
+    @property
+    def packet_data(self) -> np.ndarray:
+        return self.packet.data
+
+    @property
+    def symbol_data(self) -> np.ndarray:
+        return self.symbol.data
+
 
 
 
