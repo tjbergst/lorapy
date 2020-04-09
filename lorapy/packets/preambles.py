d9f64e198744da031719d4b7c3385237cf92261f
tjbergst <42529581+tjbergst@users.noreply.github.com>
2020-04-09 16:53:54 -0700

    new for preamble window symbol extraction

---
+++ lorapy/packets/preambles.py
@@ -0,0 +1,9 @@
+# sliced preamble window symbol slicing magic thing
+
+import numpy as np
+import typing as ty
+import matplotlib.pyplot as plt
+
+from lorapy.symbols.baseline import BaselineSymbolSet
+# from lorapy.packets.packet import LoraPacket  # TODO: circ import issue
+from lorapy.symbols import utils
